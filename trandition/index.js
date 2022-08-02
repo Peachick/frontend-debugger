02c5868fbc42de952d282289f39647cf8e9fd48b
Peachick <peachick@aliyun.com>
2022-08-02 09:13:13 +0800

    更新笔记

---
+++ trandition/index.js
@@ -0,0 +1,16 @@
+const button = document.querySelector('#btn')
+
+button?.addEventListener('click', clickCB);
+function clickCB(e) {
+  console.log('other....');
+  console.log('other....');
+  console.log('other....');
+  console.log('other....');
+  console.log(e);
+}
+
+
+const add = (x, y) => {
+  x = 2 * x;
+  return x + y;
+}
